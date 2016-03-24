327cb7545c8cb7520fd276342646e7cfa500df6c
Nolic0321 <Nolic0321@gmail.com>
2016-03-24 15:42:52 -0500

    Create views for players and storage Populates Player and Storage data Create PlayerPref class to hand PlayerPref data manipulation

--- Roguelands Helper/Form1.cs
+++ Roguelands Helper/Form1.cs
@@ -14,48 +14,110 @@ namespace Roguelands_Helper
     public partial class Form1 : Form
     {
         PlayerPrefParser fp;
+        PlayerPrefs playerPrefs;
+        DataTable allPrefs;
 
 
         public Form1()
         {
             InitializeComponent();
             fp = new PlayerPrefParser();
+            playerPrefs = new PlayerPrefs();
+            allPrefs = new DataTable();
         }
 
         private void btnLoadPlayerPrefs_Click(object sender, EventArgs e)
         {
-            Dictionary<string, PrefValue> playerPrefs = fp.Parse();
-            DataTable dt = new DataTable();
-            dt.Columns.Add("Key", typeof(string));
-            dt.Columns.Add("Value", typeof(string));
-            dt.Columns.Add("Type", typeof(string));
+            playerPrefs.playerPrefs = fp.Parse();
+            PopulateMain();
+            PopulatePlayerData();
+            PopulateStorageData();
 
-            playerPrefs.ToList()
-                .ForEach(kvp => dt.Rows.Add(new object[] { kvp.Key, kvp.Value.value, kvp.Value.type }));
-            dgvMainView.DataSource = dt;
+            dgvPlayer0.CellValueChanged += new DataGridViewCellEventHandler(RefreshMainDataGrid);
+            dgvPlayer1.CellValueChanged += new DataGridViewCellEventHandler(RefreshMainDataGrid);
+            dgvPlayer2.CellValueChanged += new DataGridViewCellEventHandler(RefreshMainDataGrid);
+            dgvPlayer3.CellValueChanged += new DataGridViewCellEventHandler(RefreshMainDataGrid);
+            dgvPlayer4.CellValueChanged += new DataGridViewCellEventHandler(RefreshMainDataGrid);
+            dgvPlayer5.CellValueChanged += new DataGridViewCellEventHandler(RefreshMainDataGrid);
+            dgvStorageChest.CellValueChanged += new DataGridViewCellEventHandler(RefreshMainDataGrid);
+        }
+
+        private void PopulateMain()
+        {
+            allPrefs = playerPrefs.GetAllPrefsDataTable();
+            dgvMainView.DataSource = allPrefs;
             dgvMainView.Columns[1].ReadOnly = false;
+        }
+
+        public void PopulatePlayerData()
+        {
+            string[] playerNums = { "0", "1", "2", "3", "4", "5" };
+
+            foreach (string playernum in playerNums)
+            {
+                DataTable temp = playerPrefs.GetPlayerDataDataTable(playernum);
+                TabPage tp = (TabPage)this.Controls.Find("tpPlayer" + playernum, true).FirstOrDefault();
+                DataGridView dgv = (DataGridView)this.Controls.Find("dgvPlayer" + playernum, true).FirstOrDefault();
+                dgv.DataSource = temp;
+
+                if (temp.Rows.Count > 0)
+                {
+                    DataRow dr = temp.Select("Key LIKE '*name'").FirstOrDefault();
+                    if (dr == null)
+                    {
+                        tp.Text = "Empty";
+                        tp.Enabled = false;
+                        continue;
+                    }
+                    tp.Text = dr[1].ToString();
+                    tp.Enabled = true;
+                    dgv.Columns[1].ReadOnly = false;
+                }
+                else
+                {
+                    tp.Text = "Empty";
+                    tp.Enabled = false;
+                }
+            }
+
+        }
+
+        public void PopulateStorageData()
+        {
+            DataTable temp = playerPrefs.GetStorageData();
+            dgvStorageChest.DataSource = temp;
+            dgvStorageChest.Columns[1].ReadOnly = false;
+
         }
 
         private void btnExport_Click(object sender, EventArgs e)
         {
             File.Copy(fp.prefLocation, fp.prefLocation + ".backup", true);
-
-            Dictionary<string, PrefValue> dict = new Dictionary<string, PrefValue>();
-            DataTable dt = (DataTable)dgvMainView.DataSource;
-            dt.AsEnumerable()
-                .ToList()
-                .ForEach(row => dict.Add(row[0] as string, new PrefValue(row[1] as string, row[2] as string)));
-
+            
             StringBuilder sb = new StringBuilder();
 
-            foreach (string key in dict.Keys)
+            foreach (string key in playerPrefs.playerPrefs.Keys)
             {
                 PrefValue value;
-                if (dict.TryGetValue(key, out value))
+                if (playerPrefs.playerPrefs.TryGetValue(key, out value))
                     sb.Append(key + " : " + value.value + " : " + value.type + " ; ");
             }
             sb.Remove(sb.Length - 2, 1);
             File.WriteAllText(fp.prefLocation, sb.ToString().Trim());
+        }
+
+        private void dgvPlayer5_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void RefreshMainDataGrid(object sender, DataGridViewCellEventArgs e)
+        {
+            foreach(DataRow row in (DataGridView)sender.Rows)
+            {
+
+            }
+            return;
         }
     }
 }
---
+++ Roguelands Helper/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelands_Helper
+{
+    class Item
+    {
+        public int id { get; set; }
+        public int quantity { get; set; }
+        public int tier { get; set; }
+        public int exp { get; set; }
+        public int attribute1 { get; set; }
+        public int attribute2 { get; set; }
+        public int attribute3 { get; set; }
+    }
+}
